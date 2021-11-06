ebccab065c09f67980391e75cd38dc31d9d2f813
suhailvs <suhailvs@gmail.com>
2021-11-06 17:56:48 +0530

    added chapters

--- django_site/torah/templatetags/torah_filters.py
+++ django_site/torah/templatetags/torah_filters.py
@@ -29,6 +29,14 @@ def get_hebrewletter(paleoletter):
     HEBREW_UNICODE = ['\u05d0','\u05d1','\u05d2','\u05d3','\u05d4','\u05d5','\u05d6','\u05d7','\u05d8','\u05d9','\u05db','\u05dc','\u05de','\u05e0','\u05e1','\u05e2','\u05e4','\u05e6','\u05e7','\u05e8','\u05e9','\u05ea']
     return HEBREW_UNICODE[PATTERN.index(paleoletter)]
 
+@register.filter(name='get_michigan_clairmont')
+def get_michigan_clairmont(p):
+    import re
+    r={'e':'h','f':'w','h':'x','j':'+','i':'y','x':'s','s':'c','c':'$'}
+    mc = re.sub(r'|'.join(r.keys()), lambda match: r[match.group(0)], p)
+    return mc.upper()
+
+
 @register.filter(name='replace_fie')
 def replace_fie(paleoword):
     """
